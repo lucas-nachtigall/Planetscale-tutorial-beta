1d7fab5ffff2ea50ed73ae991336f2be35566353
lucas Garcia Nachtigall <lucas.nachtigall@gmail.com>
2022-12-28 10:44:41 -0300

    select query

--- api/core/index.js
+++ api/core/index.js
@@ -1,7 +1,17 @@
+const mysql = require('mysql2/promise')
+
 exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello World!!'),
-    };
-    return response;
+    try {
+        const connection = await mysql.createConnection(process.env.DATABASE_URL);
+        const [rows] = await connection.execute('SELECT * FROM users');
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                rows,
+            }),
+        };
+    } catch (error) {
+        console.error(error);
+    }
 };
